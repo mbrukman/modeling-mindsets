727126bcf5e7d5a662ba465799dd520a56465dd9
Christoph Molnar <christoph.molnar@gmail.com>
2022-04-24 09:28:42 +0200

    remove script

--- _build.sh
+++ _build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e # Exit with nonzero exit code if anything fails
 
-# Create datasets
-Rscript scripts/prepare_data.R
-
 cd manuscript
 # Create references
 make -B 11-references.Rmd
