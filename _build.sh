477bcbeb59bd99ea20a211d6054ef35f00456359
Christoph Molnar <christoph.molnar@gmail.com>
2022-04-25 14:41:20 +0200

    fix references

--- _build.sh
+++ _build.sh
@@ -3,7 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 cd manuscript
 # Create references
-make -B 11-references.Rmd
+make -B references.Rmd
 # Compile pdf version of book for leanpub
 make pdf
 # Compile web version of book
--- scripts/references.R
+++ scripts/references.R
@@ -1,11 +1,10 @@
 library("data.table")
 library("readr")
-# load r packages so they are cited
-devtools::load_all()
+
 # relative project path to the .Rmd files
 path = "./"
 # name of the file where to write the references to
-out.file = "11-references.Rmd"
+out.file = "references.Rmd"
 
 # Function to collect list of .Rmd files
 get_file_list = function(folder){
@@ -17,8 +16,9 @@ grep_references = function(file){
   # read in file as string
   lines = readr::read_lines(file)
   # grep lines starting with[^XXX]:
-  lines = lines[grep("\\[\\^[a-z;A-Z;0-9;\\-]*\\]\\:", lines)]
-  # split by first :
+  lines = lines[grep("^\\[\\^[a-z;A-Z;0-9;\\-]*\\]\\:", lines)]
+  if(length(lines) == 0) return(NULL)
+  #a split by first :
   splitted = strsplit(lines, "]: ")
   # store in data.frame with key and ref
   res = data.table::rbindlist(lapply(splitted,
@@ -31,30 +31,6 @@ grep_references = function(file){
 }
 
 
-# Adapted from knitr::write_bib
-# For the citation of the R packages
-get_R_bib = function (tweak = TRUE, width = NULL){
-  lib = packageDescription("iml.book")
-  x = unlist(strsplit(lib$Import, ",\\n"))
-  x = c(x, "knitr")
-  bib = sapply(x, function(pkg) {
-    cite = citation(pkg, auto = if (pkg == "base")
-      NULL
-      else TRUE)
-    if (tweak) {
-      cite$title = gsub(sprintf("^(%s: )(\\1)", pkg), "\\1",
-        cite$title)
-      cite$title = gsub(" & ", " \\\\& ", cite$title)
-    }
-    cite
-  },
-  simplify = FALSE)
-  bib = bib[sort(x)]
-  invisible(bib)
-}
-
-
-
 
 file_list = get_file_list(path)
 file_list = setdiff(file_list, paste(path, "interpretable-ml.Rmd", sep = "/"))
@@ -64,18 +40,10 @@ reference_list = data.table::rbindlist(lapply(file_list, grep_references),
 reference_list = unique(reference_list)
 reference_list = reference_list[order(reference_list$reference), ]
 
-r_reference_list = get_R_bib()
-
 fileConn <- file(paste(path, out.file, sep = "/"))
 write_string = c("# References {-}",
   "<!-- Generated automatically, please don't edit manually! -->")
 for ( i in 1:nrow(reference_list)) {
   write_string = c(write_string, "", reference_list$reference[i])
-}
-write_string = c(write_string, "",  "## R Packages Used {-}", "")
-for ( i in 1:length(r_reference_list)) {
-  r_package_citation = paste0("**", names(r_reference_list[i]), "**. ",
-    format(r_reference_list[[i]], "textVersion", collapse = TRUE)[[1]])
-  write_string = c(write_string, "", r_package_citation)
 }
 write_lines(write_string, fileConn)
